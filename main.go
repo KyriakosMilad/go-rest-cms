ce82796212369fca20f12827bd407ef70fb2728a
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-04-26 11:35:35 +0200

    Connect to the database

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/joho/godotenv"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -13,12 +16,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// load .env variables
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file: " + err.Error())
 	}
 
-	println(os.Getenv("NAME"))
+	// connect to to the database
+	DB, err := sql.Open("mysql", os.Getenv("DB_USER")+":"+os.Getenv("DB_PASSWORD")+"@tcp("+os.Getenv("DB_HOST")+")/"+os.Getenv("DB_NAME"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(DB)
 
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":3000", nil))
