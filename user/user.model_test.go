8acd33c6f5b1518c3f6207b55133bcf9c5b418ba
Kyriakos Milad <contact.kyriakos@gmail.com>
2021-05-08 21:44:41 +0200

    Restructure user.model_test

--- user/user.model_test.go
+++ user/user.model_test.go
@@ -6,8 +6,14 @@ import (
 	"log"
 	"os"
 	"testing"
-	"time"
 )
+
+var u = User{
+	FirstName: "Kyriakos",
+	LastName:  "Milad",
+	Email:     "contact.kyriakos@gmail.com",
+	Password:  "0123456",
+}
 
 func TestMain(m *testing.M) {
 	// load .env variables
@@ -26,44 +32,17 @@ func TestMain(m *testing.M) {
 }
 
 func TestUser_Create(t *testing.T) {
-	type fields struct {
-		ID        int64
-		FirstName string
-		LastName  string
-		Email     string
-		Password  string
-		CreatedAt time.Time
-		UpdatedAt time.Time
-		DeletedAt time.Time
-	}
 	tests := []struct {
 		name    string
-		fields  fields
 		wantErr bool
 	}{
 		{
-			name: "test create user",
-			fields: fields{
-				FirstName: "Kyriakos",
-				LastName:  "Milad",
-				Email:     "contact.kyriakos@gmail.com",
-				Password:  "0635156",
-				CreatedAt: time.Now(),
-				UpdatedAt: time.Now(),
-			},
+			name:    "test create user",
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			u := User{
-				FirstName: tt.fields.FirstName,
-				LastName:  tt.fields.LastName,
-				Email:     tt.fields.Email,
-				Password:  tt.fields.Password,
-				CreatedAt: tt.fields.CreatedAt,
-				UpdatedAt: tt.fields.UpdatedAt,
-			}
 			if err := u.Create(); (err != nil) != tt.wantErr {
 				t.Errorf("Create() error = %v, wantErr %v", err, tt.wantErr)
 			}
