d3a5a8f32f4e1d118428b5d3298ce848d7eb76ca
Kyriakos Milad <contact.kyriakos@gmail.com>
2022-04-14 13:11:31 +0200

    user.model: Rename json fields

--- user/user.model.go
+++ user/user.model.go
@@ -11,13 +11,13 @@ import (
 
 type User struct {
 	ID        int64     `json:"id" db_column_name:"id" db_column_specs:"INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY"`
-	FirstName string    `json:"first_name" db_column_name:"first_name" db_column_specs:"VARCHAR(10) NOT NULL"`
-	LastName  string    `json:"last_name" db_column_name:"last_name" db_column_specs:"VARCHAR(10) NOT NULL"`
+	FirstName string    `json:"firstName" db_column_name:"first_name" db_column_specs:"VARCHAR(10) NOT NULL"`
+	LastName  string    `json:"lastName" db_column_name:"last_name" db_column_specs:"VARCHAR(10) NOT NULL"`
 	Email     string    `json:"email"  db_column_name:"email" db_column_specs:"VARCHAR(255) NOT NULL UNIQUE"`
 	Password  string    `json:"password"  db_column_name:"password" db_column_specs:"VARCHAR(255) NOT NULL"`
-	CreatedAt time.Time `json:"created_at" db_column_name:"created_at" db_column_specs:"TIMESTAMP NOT NULL"`
-	UpdatedAt time.Time `json:"updated_at" db_column_name:"updated_at" db_column_specs:"TIMESTAMP NOT NULL"`
-	DeletedAt time.Time `json:"deleted_at" db_column_name:"deleted_at" db_column_specs:"TIMESTAMP"`
+	CreatedAt time.Time `json:"createdAt" db_column_name:"created_at" db_column_specs:"TIMESTAMP NOT NULL"`
+	UpdatedAt time.Time `json:"updatedAt" db_column_name:"updated_at" db_column_specs:"TIMESTAMP NOT NULL"`
+	DeletedAt time.Time `json:"deletedAt" db_column_name:"deleted_at" db_column_specs:"TIMESTAMP"`
 }
 
 func (u User) GetTable() string {
